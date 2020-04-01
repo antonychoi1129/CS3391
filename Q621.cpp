407d5654452f5b1815e6408bfbf01bc2ac4c1239
antonychoi1129 <antonychoi1129@gmail.com>
2020-04-01 13:43:02 +0800

    Update Q621.cpp

--- Q621.cpp
+++ Q621.cpp
@@ -5,6 +5,28 @@ using namespace std;
 
 vector<int> Set;
 
+struct destroy{
+    int from, to;
+    int weight;
+    destroy(){}
+    destroy(int from, int to, int weight){
+        this->from = from;
+        this->to = to;
+        this->weight = weight;
+    }
+};
+
+struct add{
+    int from, to;
+    int weight;
+    add(){}
+    add(int from, int to, int weight){
+        this->from = from;
+        this->to = to;
+        this->weight = weight;
+    }
+};
+
 void UnionSet(int set1, int set2){
 	Set[set1] += Set[set2]; 
 	Set[set2] = set1;     
@@ -17,71 +39,111 @@ int Find(int element){
         return Set[element] = Find(Set[element]);
 }
 
-void Union (int element1, int element2) {
-	int root1 = Find(element1);
-	int root2 = Find(element2);
-	if  (Set[root1] < Set[root2]) 	
-        UnionSet(root1, root2);	
-    else		
+void Union (int root1, int root2, int edges[]){
+	if(Set[root1] < Set[root2]){
+        UnionSet(root1, root2);
+        edges[root1]++;
+    }	
+    else{
         UnionSet(root2, root1);
-}
-
-int remove(int n, int connected,  int dijointSet_num, vector<vector<bool>> c, vector<vector<int>> b,  vector<vector<int>> d){
-    int cost = 0;
-   
-    for(int i=1; i <= n; i++){
-        if(Set[i] < 0){
-            
-        }
+        edges[root2]++;
     }
 }
 
-int add(int n, int connected,  int dijointSet_num, vector<vector<bool>> c, vector<vector<int>> b,  vector<vector<int>> d){
-    int cost = 0;
+bool CMP(add a, add b){
+    return a.weight < b.weight;
+}
+
+bool CMP1(destroy a, destroy b){
+    return a.weight < b.weight;
 }
 
 int main(){
     int n;
     while(cin >> n){
-        vector<vector<bool>> c(n+1, vector<bool>(n+1));
-        vector<vector<int>> b(n+1, vector<int>(n+1));
-        vector<vector<int>> d(n+1, vector<int>(n+1));
+        if(n == 0) break;
+
+        vector<add> b;
+        vector<destroy> d;
+        int edges[n+1];
 
         Set.resize(n+1);
-        for(int i =1; i <= p; i++)
+        for(int i = 1; i <= n; i++){
             Set[i] = -1;
-
-        int connected = 0;
+            edges[i] = 0;
+        }
+            
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
-                cin >> c[i][j];
-                if(c[i][j] == 1 && j > i){
-                    Union(i,j);
-                    connected++;
+                char c;
+                cin >> c;
+                if(c == '1' && j > i){
+                    int root1 = Find(i);
+                    int root2 = Find(j);
+                    if(root1 != root2){
+                        Union(root1, root2, edges);
+                    }
+                    else {
+                        edges[root1]++;
+                    }
+                    	
+                }
+            }
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                int temp;
+                cin >> temp;
+                if(j > i){
+                    b.push_back(add(i, j, temp));
                 }
             }
         }
 
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
-                cin >> b[i][j];
+                int temp;
+                cin >> temp;
+                if(j > i)
+                    d.push_back(destroy(i, j, temp));
             }
         }
-
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                cin >> d[i][j];
-            }
-        }
-
-        int dijointSet_num = 0;
-        for(int i=1; i <= n; i++){
-            if(Set[i] < 0) dijointSet_num++;
-        }
-
+        // for(int i = 1; i <= n; i++)
+        //     cout << "ed " << edges[i] << endl;
         int rebuild = 0;
-        rebuild += remove(n, connected, dijointSet_num, c, b, d);
-        rebuild += add(n, connected, dijointSet_num, c, b, d);
+        sort(b.begin(), b.end(), CMP);
+        for(int i = 0; i < b.size(); i++){
+            int root1 = Find(b[i].from);
+	        int root2 = Find(b[i].to);
+            if(root1 != root2){
+                Union(root1, root2, edges);
+                rebuild += b[i].weight;
+                if(Set[root1] < Set[root2]){
+                    cout << i << " " << edges[root1] << " " << Set[root2] << endl;
+                    edges[root1] += Set[root2]; 
+                }	
+                else{
+                    edges[root2] += Set[root1];
+                    cout << i << " " <<edges[root2] << " " << Set[root1] << endl;
+
+    }
+            }
+        }
+        
+// for(int i = 1; i <= n; i++)
+//             cout << "   ed " << edges[i] << endl;
+        sort(d.begin(), d.end(), CMP1);
+        for(int i = 0; i < d.size(); i++){
+            int root = Find(d[i].from);
+            // cout << "condi " << root << " " << d[i].from << " " <<  d[i].to << endl;
+            // cout << "compare " << edges[root] << " " <<  -Set[root]-1 << endl;
+            if(edges[root] > (-Set[root]) - 1){
+                // cout << "remove " << root << " " << d[i].from << " " <<  d[i].to << endl;
+                rebuild += d[i].weight;
+                edges[root]--;
+
+            }
+        }
         cout << rebuild << endl;
     }
 }
