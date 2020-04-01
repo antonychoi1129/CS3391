d4e0d78402bee2846255a6ba57e03fd270d6bb41
antonychoi1129 <antonychoi1129@gmail.com>
2020-04-01 13:42:41 +0800

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,39 +1,22 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<queue>
+#include<algorithm>
 using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int t, n, i, score, j;
-    string s;
-    cin >> t;
-    while(t--){
-        vector<int> team;
-        cin >> n;
-        for(i =0; i < 3*n; i++){
-            cin >> score;
-            team.push_back(score);
-        }
-
-       
-        for (i = 0; i < team.size()-1; i++)      
-            for (j = 0; j < team.size()-i-1; j++)  
-                if (team[j] > team[j+1])  
-                    swap(team[j], team[j+1]); 
-
-        
-        int sum = 0;
-
-        for(i =0; i <team.size();i++){
-            
-            team.pop_back();
-           
-            sum += team[team.size()-1];
-            team.pop_back();
-            
-        }
-        cout << sum << endl;
+    int L, W, N;
+    int i, j, k, cases = 0;
+    int x[105], y[105];
+    while(cin >> L >> W >> N){
+        int L, W, N, d;
+        for(i = 0; i < N; i++)
+            cin >> x[i] >> y[i];
+        int start = 2 * N, end = 2 * N + 1;
     }
     return 0;
-}
+} 
