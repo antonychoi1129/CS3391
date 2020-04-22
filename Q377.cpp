fe43b20b73430b3e6c3933e89013186ea0ed6fad
antonychoi1129 <antonychoi1129@gmail.com>
2020-04-22 21:12:30 +0800

    Update Q377.cpp

--- Q377.cpp
+++ Q377.cpp
@@ -1,73 +1,133 @@
 #include<iostream>
 #include<vector>
-
+#include<queue>
+#include<cstring>
 using namespace std;
 
-int i, n, m, pl, from, to;
+struct FlowEdge {
+    int v, u;
+    long long cap, flow;
+    FlowEdge(int from, int to, long long c){
+        v = from;
+        u = to;
+        cap = c;
+        // flow = 0;
+    } 
+};
 
-long sumProfit(int e, int adj_num[], int net[], long sum[], bool visited[], vector<vector<bool>> adj){
-    if(visited[e]){
-        cout << "return " << 0 << endl;
-        return 0;
+const long long flow_inf = 1e18;
+int n, m = 0;
+struct Dinic {
+    vector<FlowEdge> edges;
+    vector<vector<int> > adj;
+    int s, t;
+    vector<int> level, ptr;
+    queue<int> q;
+
+    Dinic(int x, int start, int end){
+        s = start;
+        t = end;
+        n = x;
+        adj.resize(n+1);
+        level.resize(n+1);
+        ptr.resize(n+1);
     }
-        
-    else{
-        cout << "hit " << e << endl;
-        bool found = false;
-        visited[e] = true;
-        for(int i=1; i < n+1; i++){
-            if(adj[e][i]){
-                found = true;
-                adj_num[e]++;
-                sum[e] += sumProfit(i, adj_num, net, sum, visited, adj);
-                cout << "sum " << e << " " << sum[e] << endl;
-            }
-        }
-        
-        
-        cout << "base " << sum[e] << endl;
-        return sum[e];
-        
-            
-    }
-}
 
-int main(){
-    
-    while(cin >> n >> m){
-        int net[n+1];
-        for(i = 1; i < n+1; i++){
-            cin >> pl;
-            net[i] = pl;
-        }
-        vector<vector<bool>> adj(n+1, vector<bool>(n+1, 0));
-        for(i=0; i<m; i++){
-            cin >> from >> to;
-            adj[from][to] = true;
-        }
-        for(i=1; i<n+1; i++){
-            for(int j=1; j<n+1; j++){
-                cout << adj[i][j] << " ";
-            }
-            cout << endl;
-        }
-        int adj_num[n+1];
-        long sum[n+1];
-        bool visited[n+1];
-
-        for(i=1; i < n+1; i++){
-            adj_num[i] = 0;
-            sum[i] = net[i];
-            visited[i] = false;
-        }
-
-        for(i=1; i < n+1; i++){
-            visited[i] = false;
-            sum[i] = sumProfit(i, adj_num, net, sum, visited, adj);
-        }
-        for(i=1; i < n+1; i++){
-            cout << sum[i] << " ";
-        }
+    void add_edge(int v, int u, long long cap) {
+        edges.push_back(FlowEdge(v, u, cap));
+        edges.push_back(FlowEdge(u, v, 0));
+        m = edges.size();
+        adj[v].push_back(m);
+        adj[u].push_back(m + 1);
        
     }
+
+    bool bfs() {
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            for (int i = 0; i < adj[v].size(); i++) {
+                int id = adj[v][i];
+                if (edges[id].cap - edges[id].flow < 1)
+                    continue;
+                if (level[edges[id].u] != -1)
+                    continue;
+                level[edges[id].u] = level[v] + 1;
+                q.push(edges[id].u);
+            }
+        }
+        return level[t] != -1;
+    }
+
+    long long dfs(int v, long long pushed) {
+        if (pushed == 0)
+            return 0;
+        if (v == t)
+            return pushed;
+        for (int& cid = ptr[v]; cid < (int)adj[v].size(); cid++) {
+            int id = adj[v][cid];
+            int u = edges[id].u;
+            if (level[v] + 1 != level[u] || edges[id].cap - edges[id].flow < 1)
+                continue;
+            long long tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+            if (tr == 0)
+                continue;
+            edges[id].flow += tr;
+            edges[id ^ 1].flow -= tr;
+            return tr;
+        }
+        return 0;
+    }
+
+    long long flow() {
+        long long f = 0;
+        while (true) {
+            fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            q.push(s);
+            if (!bfs())
+                break;
+            fill(ptr.begin(), ptr.end(), 0);
+            while (long long pushed = dfs(s, flow_inf)) {
+                f += pushed;
+            }
+        }
+        return f;
+    }
+};
+int main(){
+    int x,y;
+	while(cin >> x >> y){
+		int cnt = 0;
+		int i, j, a, b, c;
+		
+		int sum = 0;
+		int maxProfit = 0;
+		
+		int start = 0, end = x + 1;
+		Dinic d(end+1, start, end);
+		for (i = 1; i <= x; i++)
+		{
+			cin >> c;
+			if (c > 0) {
+				d.add_edge(start, i, c);
+				sum += c;
+			}
+			else if(c<0){
+				d.add_edge(i, end, -c);
+			}
+			
+		}
+		for (i = 1; i <= y; i++)
+		{
+			cin >> a >> b;
+			d.add_edge(a, b , flow_inf);
+		}
+		long long flow = d.flow();
+        cout << sum << endl;
+        cout << flow << endl;
+		maxProfit = sum - flow;
+		// cout << d.cnt()-1 << " " << maxProfit << endl;
+        cout << maxProfit << endl;
+	}
 }
