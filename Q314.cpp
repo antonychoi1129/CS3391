eeefdd688d8df31fa2a270905652955994c5cb1a
antonychoi1129 <antonychoi1129@gmail.com>
2020-05-12 20:32:59 +0800

    Update Q314.cpp

--- Q314.cpp
+++ Q314.cpp
@@ -11,30 +11,60 @@ string reverse(string s){
     return rev;
 }
 
-int findPalindromes(string s){
+int KMP(string s){
     int len = s.length();
-    int x = len - 1;
-    for(int i=len-2; i >=0 ; i--){
-        string sub = s.substr(i, len - i);
-        if(sub.compare(reverse(sub)) == 0){
-            x = i;
+    int lps[len];
+    int l = 0;
+    lps[0] = 0;
+    string p = reverse(s);
+    int i,j;
+    i = 1;
+    while(i < len){
+        if(p[i] == p[l]){
+            l++;
+            lps[i] = l;
+            i++;
+        }
+        else{
+            if(l != 0){
+                l = lps[l-1];
+            }
+            else{
+                lps[i] = 0;
+                i++;
+            }
         }
     }
-    return x;
+
+    i = j = 0;
+    int index = 0;
+    while(i < len){
+        if(p[j] == s[i]){
+            if(i == len-1){
+                index = i-j;
+            }
+            i++; j++;
+        } 
+        else if(i < len){
+            if(j != 0) 
+                j = lps[j-1];
+            else
+                i++;
+        }
+    }
+    return index;
 }
+
 int main(){
     string input;
     int x;
     while(cin >> input){
-        x = findPalindromes(input);
+        x = KMP(input);
         cout << input;
         x--;
         for(int i = x; i >= 0; i--){
                 cout << input[i];
         }
-        
-        
         cout << endl;
     }
-      
 }
